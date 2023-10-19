4b42e984c6b3a2da75de2597b3e51024e01404e2
Anik Chakraborty <cani104@gmail.com>
2023-10-19 17:23:08 +0530

    Added observable variable for search transporter in post load details screen

--- lib/controller/addLocationDrawerToggleController.dart
+++ lib/controller/addLocationDrawerToggleController.dart
@@ -3,6 +3,11 @@ import 'package:get/get.dart';
 class AddLocationDrawerToggleController extends GetxController {
   RxBool drawerState = false.obs;
   RxBool addTransporter = false.obs;
+  RxString searchTransporterText = ''.obs;
+
+  updateSearchTransporterText(String txt){
+    searchTransporterText.value = txt;
+  }
 
   toggleDrawer(bool selectedState) {
     drawerState.value = selectedState;
