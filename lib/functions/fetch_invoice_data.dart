126a3a74b7684bc2f8f7209b351c7bcd2c167d35
Albin Augusty <2003010019@ipec.org.in>
2023-12-11 21:02:55 +0530

    implemented invoice

--- lib/functions/fetch_invoice_data.dart
+++ lib/functions/fetch_invoice_data.dart
@@ -3,14 +3,18 @@ import 'package:http/http.dart' as http;
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class ApiService {
-  static Future<List<dynamic>> getInvoiceData(String shipperId) async {
+  static Future<List<dynamic>> getInvoiceData(
+      String shipperId, String from, String to) async {
     final String invoiceApi = dotenv.get('invoiceApiUrl');
-    final response = await http.get(Uri.parse('$invoiceApi?shipperId=$shipperId'));
+    final response = await http.get(Uri.parse(
+        '$invoiceApi?shipperId=$shipperId&fromTimestamp=$from&toTimestamp=$to'));
 
     if (response.statusCode == 200) {
       List<dynamic> data = json.decode(response.body);
       return data;
     } else {
+      print('Error Status Code: ${response.statusCode}');
+      print('Error Response Body: ${response.body}');
       throw Exception('Failed to load invoice');
     }
   }
