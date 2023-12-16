1d0608bb0a2d42a5197a405f46fc362b3a16fee4
Aman Gautam <amangautam1905@gmail.com>
2023-12-16 21:12:20 +0530

    Added the roles and companyId to the send invite button

--- lib/screens/add_user_screen.dart
+++ lib/screens/add_user_screen.dart
@@ -21,8 +21,10 @@ class _AddUserState extends State<AddUser> {
   late String phoneOrMail;
   bool isError = false;
   ShipperIdController shipperIdController = Get.put(ShipperIdController());
-  String selectedRole = "employee";
-  
+  String selectedRole = "EDITOR";
+
+  List<String> dateRanges = ["ADMIN", "EDITOR", "VIEWER"];
+
   @override
   Widget build(BuildContext context) {
     final screenWidth = MediaQuery.of(context).size.width;
@@ -35,8 +37,10 @@ class _AddUserState extends State<AddUser> {
         child: SingleChildScrollView(
           child: ConstrainedBox(
             constraints: BoxConstraints(
-                maxHeight: Responsive.isMobile(context) ?  screenHeight * 0.4  : screenHeight * 0.45,
-                maxWidth:  screenWidth * 0.7 ),
+                maxHeight: Responsive.isMobile(context)
+                    ? screenHeight * 0.4
+                    : screenHeight * 0.45,
+                maxWidth: screenWidth * 0.7),
             child: Form(
               key: _formKey,
               child: Column(
@@ -53,59 +57,133 @@ class _AddUserState extends State<AddUser> {
                           style: GoogleFonts.montserrat(
                               color: darkBlueTextColor,
                               fontWeight: FontWeight.w500,
-                              fontSize: Responsive.isMobile(context) ? 14.sp : 4.75.sp),
+                              fontSize: Responsive.isMobile(context)
+                                  ? 14.sp
+                                  : 4.75.sp),
                         ),
                       ),
                       Row(
                         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                         children: [
                           Expanded(
-                            flex: 817,
+                            flex: 648,
                             child: Padding(
-                              padding: Responsive.isMobile(context) ? 
-                              const EdgeInsets.only(left: 20.0, top: 30, bottom: 30) 
-                              : const EdgeInsets.only(left: 54.0, top: 30, bottom: 30),
-                              child: TextFormField(
-                                decoration: InputDecoration(
-                                  labelText: 'Email',
-                                  floatingLabelAlignment:
-                                      FloatingLabelAlignment.start,
-                                  floatingLabelStyle: GoogleFonts.montserrat(
-                                      color: darkBlueTextColor,
-                                      fontWeight: FontWeight.w500),
-                                  hintText: 'Enter Email Address ',
-                                  border: const OutlineInputBorder(
-                                    borderRadius:
-                                        BorderRadius.all(Radius.circular(2)),
-                                  ),
+                              padding: Responsive.isMobile(context)
+                                  ? const EdgeInsets.only(
+                                      left: 20.0, top: 30, bottom: 30)
+                                  : const EdgeInsets.only(
+                                      left: 54.0, top: 30, bottom: 30),
+                              child: Container(
+                                alignment: Alignment.centerLeft,
+                                padding:
+                                    EdgeInsets.only(left: screenWidth * 0.01),
+                                decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(7),
+                                    boxShadow: [
+                                      BoxShadow(
+                                        color: Colors.grey.withOpacity(0.2),
+                                        spreadRadius: 5,
+                                        blurRadius: 5,
+                                        offset: const Offset(0, 3),
+                                      ),
+                                    ],
+                                    color: white),
+                                child: TextFormField(
+                                  decoration: InputDecoration(
+                                      fillColor: Colors.white,
+                                      hintText: 'Enter Email Address',
+                                      hintStyle: GoogleFonts.montserrat(
+                                          fontWeight: FontWeight.w500,
+                                          color: grey2,
+                                          fontSize: Responsive.isMobile(context)
+                                              ? screenHeight * 0.027
+                                              : screenWidth * 0.012),
+                                      border: InputBorder.none),
+                                  validator: (value) {
+                                    if (value == null || value.isEmpty) {
+                                      return 'Please enter an email address';
+                                    } else if (!RegExp(
+                                            r'\b[\w\.-]+@[\w\.-]+\.\w{2,4}\b')
+                                        .hasMatch(value)) {
+                                      return 'Please enter a valid email address';
+                                    }
+                                    return null;
+                                  },
+                                  onSaved: (value) {
+                                    phoneOrMail = value.toString();
+                                  },
                                 ),
-                                validator: (value) {
-                                  setState(() {
-                                    isError = false;
-                                  });
-                                  return null;
-                                },
-                                onSaved: (value) {
-                                  phoneOrMail = value.toString();
-                                },
                               ),
                             ),
                           ),
                           Expanded(
+                              flex: 250,
+                              child: Container(
+                                height: 55,
+                                margin: EdgeInsets.only(
+                                    left: screenWidth * 0.008),
+                                decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(7),
+                                    boxShadow: [
+                                      BoxShadow(
+                                        color: Colors.grey.withOpacity(0.2),
+                                        spreadRadius: 5,
+                                        blurRadius: 5,
+                                        offset: const Offset(0, 3),
+                                      ),
+                                    ],
+                                    color: white),
+                                child: DropdownButtonHideUnderline(
+                                  child: DropdownButton<String>(
+                                      value: selectedRole,
+                                      dropdownColor: Colors.white,
+                                      alignment: Alignment.center,
+                                      icon: Padding(
+                                          padding: EdgeInsets.only(
+                                              right: screenWidth * 0.015),
+                                          child: const Icon(
+                                              Icons.arrow_drop_down,
+                                              size: 30)),
+                                      items: dateRanges.map((String role) {
+                                        return DropdownMenuItem<String>(
+                                          value: role,
+                                          child: Padding(
+                                            padding: EdgeInsets.only(
+                                                left: screenWidth * 0.015),
+                                            child: Text(role,
+                                                textAlign: TextAlign.center,
+                                                style: GoogleFonts.montserrat(
+                                                    fontWeight: FontWeight.w500,
+                                                    fontSize: 18,
+                                                    color: Colors.black)),
+                                          ),
+                                        );
+                                      }).toList(),
+                                      onChanged: (newValue) {
+                                        setState(() {
+                                          selectedRole = newValue!;
+                                        });
+                                      }),
+                                ),
+                              )),
+                          Expanded(
                               flex: 183,
                               child: Container(
-                                margin: Responsive.isMobile(context) 
-                                ? const EdgeInsets.only(left: 15, right: 15.0)
-                                : const EdgeInsets.only(left: 15, right: 50.0),
+                                margin: Responsive.isMobile(context)
+                                    ? const EdgeInsets.only(
+                                        left: 15, right: 15.0)
+                                    : const EdgeInsets.only(
+                                        left: 15, right: 50.0),
                                 height: 50,
                                 width: 22,
                                 decoration: BoxDecoration(
                                     borderRadius: BorderRadius.circular(2),
                                     color: darkBlueTextColor),
                                 child: Padding(
-                                  padding: Responsive.isMobile(context) 
-                                  ? EdgeInsets.symmetric(horizontal: screenWidth * 0.005) 
-                                  : const EdgeInsets.all(12.0),
+                                  padding: Responsive.isMobile(context)
+                                      ? EdgeInsets.symmetric(
+                                          horizontal: screenWidth * 0.005)
+                                      : const EdgeInsets.all(12.0),
                                   child: const Icon(
                                       size: 25, Icons.add, color: Colors.white),
                                 ),
@@ -113,13 +191,17 @@ class _AddUserState extends State<AddUser> {
                         ],
                       ),
                       SizedBox(
-                        height: Responsive.isMobile(context) ? 0.005.h :  1.9.h,
+                        height: Responsive.isMobile(context) ? 0.005.h : 1.9.h,
                       ),
                       Container(
                         height: 55,
                         child: Padding(
-                          padding:  EdgeInsets.symmetric(
-                              horizontal: Responsive.isMobile(context) ? screenWidth * 0.20 : 265.0, vertical: Responsive.isMobile(context)? 3.0 : 10.0),
+                          padding: EdgeInsets.symmetric(
+                              horizontal: Responsive.isMobile(context)
+                                  ? screenWidth * 0.20
+                                  : 265.0,
+                              vertical:
+                                  Responsive.isMobile(context) ? 3.0 : 10.0),
                           child: ElevatedButton(
                             style: ElevatedButton.styleFrom(
                               shape: RoundedRectangleBorder(
@@ -130,12 +212,12 @@ class _AddUserState extends State<AddUser> {
                             onPressed: () async {
                               if (_formKey.currentState!.validate()) {
                                 _formKey.currentState!.save();
-                                //Add the user
-                                // await AddUserFunctions().addUser(phoneOrMail,
-                                //     shipperIdController.companyName.value,
-                                //     context: context, role: selectedRole);
-                                // //Send email to the same user
-                                // AddUserFunctions().sendEmailToEmployee(phoneOrMail, shipperIdController.name.value);
+                                AddUserFunctions().sendEmailToEmployee(
+                                    phoneOrMail,
+                                    shipperIdController.companyName.value,
+                                    shipperIdController.companyId.value,
+                                    selectedRole,
+                                    context);
                               }
                             },
                             child: Row(
